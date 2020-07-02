cb88dd9d5c85c65b339e3e344c669e51aa22702e
aarneranta <aarne@chalmers.se>
2020-07-02 13:10:16 +0200

    robust datediff

--- datediff.py
+++ datediff.py
@@ -33,27 +33,71 @@ def dateDiff(year1,month1,day1,year2,month2,day2):
     diff = dateDays(year2,month2,day2) - dateDays(year1,month1,day1)
     return diff
 
-def inputDate(date):
+#### new stuff added for Lecture 6
+
+def checkDate(year,month,day):
+    return (0 < month <= 12) and (0 < day <= monthLength(year,month))
+
+def inputDate(prompt):
+    date = (input(prompt + ", format år,månad,dag: ")).split(',')
+    if len(date) == 3 and date[0].isdigit() and date[1].isdigit() and date[2].isdigit():
+        year,month,day = int(date[0]),int(date[1]),int(date[2])
+        if checkDate(year,month,day):
+            return year,month,day
+        else:
+            print("ogiltigt datum")
+            return inputDate(prompt)
+    else:
+        print("försök igen, tre tal")
+        return inputDate(prompt)
+    
+#### end new stuff
+
+### old versions step by step, not used in the actual main()
+def inputDate1(date):
     year,month,day = eval(input("Ange " + date + " i format år,månad,dag: "))
     return year,month,day
 
-def main():
-    year1,month1,day1 = inputDate("startdatum")
-    year2,month2,day2 = inputDate("slutdatum")
-    print("Skillnaden är", dateDiff(year1,month1,day1,year2,month2,day2))
+def inputDate2(prompt):
+    year,month,day = input(prompt+": ").split(',')
+    return int(year),int(month),int(day)
 
-# main()
-
-"""
-def checkDate(year,month,day):
-    if month == 0 or day == 0 or month > 12 or day > monthLength(year,month):
-        print("invalid date")
+def inputDate3(prompt):
+    date = (input(prompt + ": ")).split(',')
+    if len(date) == 3:
+        return int(date[0]),int(date[1]),int(date[2])
     else:
-        return year,month,day
+        print("ogiltigt datum")
+        return inputDate3(prompt)
 
-def inputDate(prompt):
-    year,month,day = eval(input(prompt + ", using format year,month,date: "))
-    return checkDate(year,month,day)
-"""
+def inputDate4(prompt):
+    date = (input(prompt + ": ")).split(',')
+    if (len(date) == 3
+            and date[0].isdigit()
+            and date[1].isdigit()
+            and date[2].isdigit()):
+        return int(date[0]),int(date[1]),int(date[2])
+    else:
+        print("ogiltigt datum")
+        return inputDate4(prompt)
+##########
+
+    
+def main():
+    try:
+        year1,month1,day1 = inputDate("startdatum")
+        year2,month2,day2 = inputDate("slutdatum")
+        print("Skillnaden är", dateDiff(year1,month1,day1,year2,month2,day2))
+    except KeyboardInterrupt:
+        print()
+        print("om du vill lämna programmet, tryck ^D")
+        main()
+    except:
+        print()
+        print("Ok, hejdå")
+
+if __name__ == '__main__':
+    main()
+
 
 

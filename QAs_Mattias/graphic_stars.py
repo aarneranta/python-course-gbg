91adfa96fc53a6c11b08c9612a1f5852c3d99d1d
m-ulmestrand <54723095+m-ulmestrand@users.noreply.github.com>
2020-08-03 15:32:50 +0200

    Update graphic_stars.py

--- QAs_Mattias/graphic_stars.py
+++ QAs_Mattias/graphic_stars.py
@@ -2,24 +2,27 @@ from graphics import *
 from numpy import *
 from random import randint
 
-# n: antal hörn
-# r: sidlängd
-# x_pos, y_pos: koordinater för vänstra hörnet
-# window: fönster som vi ritar i
+# n: number of corners
+# r: side length of star
+# x_pos, y_pos: coordinates for leftmost corner
+# window: the graphics window to draw in
 
 
 def draw_star(n, r, x_pos, y_pos, window):
-    # Vi säkerställer att n tas in på rätt sätt
-
+    
+    # Check so that n is received as an integer
     if not isinstance(n, int):
         n = int(n)
 
+    # If for some reason n was negative, now it is not
     n = abs(n)
     if n % 2 == 0:
         n += 1
 
+    # Random colours for some sparkle to the stars
     red, green, blue = randint(0, 255), randint(0, 255), randint(0, 255)
     for i in range(n):
+        # Decides where the next position is. Some basic trigonometry to decide the corners
         x_pos2 = (x_pos + r * (cos(i * (n-1)*pi/n)))
         y_pos2 = (y_pos + r * (sin(i * (n-1)*pi/n)))
 
@@ -36,6 +39,7 @@ n_points = input('Number of points (odd, otherwise converted to odd number):')
 
 window = GraphWin('Stars', width=400, height=400)
 
+# Draw several stars! Click on the graphics window and they will appear
 for i in range(n_stars):
     p = window.getMouse()
     draw_star(n_points, 40, p.getX(), p.getY(), window)

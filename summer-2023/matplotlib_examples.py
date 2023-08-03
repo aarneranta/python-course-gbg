4ed2301de5b28f27a21f5e3333200cad767a3a70
Arianna Masciolini <uzkamascio@gmail.com>
2023-08-03 10:23:43 +0200

    templates for ex11

--- summer-2023/matplotlib_examples.py
+++ summer-2023/matplotlib_examples.py
@@ -2,10 +2,3 @@ import matplotlib.pyplot as plt
 
 data_points = [(1,2.1), (2,3.2), (3,3.85), (4,5.15), (5,6.05)]
 y_predictions = [2,3,4,5,6]
-
-xs = [x for (x,y) in data_points]
-ys = [y for (x,y) in data_points]
-
-plt.plot(xs,ys,'ro')
-plt.plot(xs,y_predictions)
-plt.show()

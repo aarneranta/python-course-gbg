b81d7d8f9d32ae48485fc6c76d056ff289d7ed30
Arianna Masciolini <uzkamascio@gmail.com>
2023-08-03 14:50:19 +0200

    lajvkod qa 11

--- summer-2023/easy_matrix.py
+++ summer-2023/easy_matrix.py
@@ -5,37 +5,61 @@ m4 = [[1, 2, 3], [3, 4], [5, 6]]
 
 def lab3_format(m):
     '''
-    Convert a matrix in a string in the format used in lab 3
+    Print a matrix in a string in the format used in lab 3
     '''
-    pass   
+    for row in m:
+        print("\t".join([str(cell) for cell in row]))
 
 def ismat(m):
     '''
     Check that a list of list is actually a matrix
     '''
-    pass
+    row_len = len(m[0])
+    for row in m[1:]:
+        if len(row) != row_len:
+            return False
+    return True
 
 def matdim(m):
     '''
     Return the dimensions (nrows, ncols) of a given matrix
     '''
-    pass
+    return (len(m), len(m[0]))
 
 def emptymat(nrows, ncols):
     '''
-    Build an empty matrix with the given dimensions
+    Build an empty (None) matrix with the given dimensions
     '''
-    pass
+    m = []
+    for _ in range(nrows):
+        row = []
+        for _ in range(ncols):
+            row.append(None)
+        m.append(row)
+    return m
+
 
 def matmul_by_scalar(m, s):
     '''
     Multiply a matrix by a scalar
     '''
-    pass
+    (nrows, ncols) = matdim(m)
+    for i in range(nrows):
+        for j in range(ncols):
+            m[i][j] = m[i][j] * s
+    return m
+
 
 def matsum(m1, m2):
     '''
     Sum two matrices
     '''
-    pass
+    if matdim(m1) != matdim(m2):
+        return
+    (nrows, ncols) = matdim(m1)
+    m = emptymat(nrows, ncols)
+    for i in range(nrows):
+        for j in range(ncols):
+            m[i][j] = m1[i][j] + m2[i][j]
+    return m
  
--- summer-2023/matplotlib_examples.py
+++ summer-2023/matplotlib_examples.py
@@ -1,4 +1,13 @@
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import plot, title, show, legend
 
 data_points = [(1,2.1), (2,3.2), (3,3.85), (4,5.15), (5,6.05)]
 y_predictions = [2,3,4,5,6]
+
+xs = [x for (x,y) in data_points]
+ys = [y for (x,y) in data_points]
+
+plot(xs, ys, 'ro')
+plot(xs, y_predictions, label="predicted")
+title("Linear regression")
+legend()
+show()

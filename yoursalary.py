3e1de4719abd06cfc8caa466d48332936046cdcd
Aarne Ranta <aarne@chalmers.se>
2024-09-03 11:56:33 +0200

    modified mysalary example

--- yoursalary.py
+++ yoursalary.py
@@ -1,37 +1,24 @@
 # file yoursalary.py
 
-"An interactive program that computes information about the user's salary"
+def future(years, salary, incr):
+    for y in range(1, years+1):
+        salary = salary * (1 + incr/100)
+    return salary
 
-def future():
-    "computes the future development of the salary"
+
+def main():
     salary = int(input("nuvarande lön: "))
     incr = float(input("årlig ökning i procent: "))
     years = int(input("hur många år vill du se? "))
-    
-    for y in range(1,years+1):
-        salary = salary * (1 + incr/100)
-        print("år",y,":",salary)
+    mode = input("vill du se utvecklingen år för år (ja/nej)?")
 
-def remaining():
-    "computes how much is left after tax and rent"
-    salary = int(input("bruttolön: "))
-    taxrate = int(input("skatteprocent: "))
-    rent = int(input("hyra: "))
-
-    tax = salary*taxrate/100
-    net = salary - tax
-    print("efter skatt: ", net)
-    print("kvar efter skatt och hyra: ",net - rent)
-
-def main():
-    "guides the user to select different types of information requests"
-    mode = str(input("löneutveckling (u) eller kvar efter skatt och hyra (k): "))
-    if mode == "u":
-        future()
-    elif mode == "k":
-        remaining()
+    if mode == "nej":
+        print(future(years, salary, incr))
+    elif mode == "ja":
+        for y in range(1, years+1):
+            print("år", y, future(y, salary, incr))
     else:
-        print("var god svara u eller k")
+        print("var god svara ja eller nej")
 
 main()
 
